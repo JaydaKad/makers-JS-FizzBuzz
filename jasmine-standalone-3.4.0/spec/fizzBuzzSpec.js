e8af1f292e0a105fbcbe08e4d9493420c712f17b
JAYDA KADIR <jydkdr@gmail.com>
2019-06-10 15:57:22 +0100

    Can return number if not fizzbuzz

--- jasmine-standalone-3.4.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-3.4.0/spec/fizzBuzzSpec.js
@@ -6,35 +6,20 @@ describe("FizzBuzz", function() {
     fizzBuzz = new FizzBuzz();
   });
 
-  it("should return \"fizz\" when passed 3", function() {
+  it("should return \"fizz\" when divisible by 3", function() {
       expect(fizzBuzz.play(3)).toEqual('fizz');
   });
 
-  it("should return \"buzz\" when passed 5", function() {
+  it("should return \"buzz\" when divisble by 5", function() {
     expect(fizzBuzz.play(5)).toEqual('buzz');
   });
 
-  it("should return \"fizzbuzz\" when passed 15", function() {
-    expect(fizzBuzz.play(15)).toEqual('fizzbuzz');
-  });  
+  it("should return \"fizzbuzz\" when divisible by 3 and 5", function() {
+    expect(fizzBuzz.play(30)).toEqual('fizzbuzz');
+  });
+
+  it("should return the number when not divisible by 3 or 5", function() {
+    expect(fizzBuzz.play(4)).toEqual(4);
+  });
 
 });
-
-
-
-
-// it 'returns "buzz" when passed 5' do
-//     expect(fizzbuzz(5)).to eq 'buzz'
-// end
-//
-// it 'expects "fizz" is divisible by 3' do
-//     expect(fizzbuzz(6)).to eq 'fizz'
-// end
-//
-// it 'expects "buzz" is divisible by 5' do
-//     expect(fizzbuzz(10)).to eq 'buzz'
-// end
-//
-// it 'expects "fizzbuzz" is divisble by 3 and 5' do
-//       expect(fizzbuzz(15)).to eq 'fizzbuzz'
-// end
--- jasmine-standalone-3.4.0/src/fizzBuzz.js
+++ jasmine-standalone-3.4.0/src/fizzBuzz.js
@@ -2,17 +2,13 @@
 function FizzBuzz() {
 }
 FizzBuzz.prototype.play = function(number) {
-  if(number % 15 == 0){
+  if(number % 15 == 0 ){
     return 'fizzbuzz'
   } else if(number % 3 == 0) {
     return 'fizz'
+  } else if(number % 5 == 0) {
+    return 'buzz'
   } else {
-      return 'buzz'
+    return number
   }
 };
-
-// def fizzbuzz(number)
-//   return 'fizz' if number % 3 == 0 && number % 5 != 0
-//   return 'buzz' if number % 5 == 0 && number % 3 != 0
-//   return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
-// end
